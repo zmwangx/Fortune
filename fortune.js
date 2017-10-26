4fa9f677bedf8e53d2f359b3f66a33232ae55549
Zhiming Wang <zmwangx@gmail.com>
2017-10-26 09:38:19 -0400

    Preserve fortune on the use of back button
    Occasionally I get a glimpse of an interesting fortune as I press enter in the
    address bar; now I have a way to restore it and read more closely.

--- fortune.js
+++ fortune.js
@@ -1,4 +1,19 @@
 const FORTUNES = document.getElementById('fortune-data').text.split('\n%\n');
 const NUM_FORTUNES = FORTUNES.length - 1;  // The last element is an empty string.
+const fortuneTag = document.getElementById('fortune');
+var fortuneId = 0;
 
-document.getElementById('fortune').textContent = FORTUNES[Math.floor(Math.random() * NUM_FORTUNES)];
+function randomId () {
+  return Math.floor(Math.random() * NUM_FORTUNES);
+}
+
+if (window.location.hash !== '') {
+  fortuneId = parseInt(window.location.hash.substr(1));
+  if (isNaN(fortuneId) || fortuneId < 0 || fortuneId >= NUM_FORTUNES) {
+    fortuneId = randomId();
+  }
+} else {
+  fortuneId = randomId();
+}
+fortuneTag.textContent = FORTUNES[fortuneId];
+window.location.hash = fortuneId.toString();
